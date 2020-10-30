4d9fdaf78b72f1d7d21195eaf1e21ec0e960ccc9
Isac Canedo <32867124+isaccanedo@users.noreply.github.com>
2020-10-30 19:05:36 -0300

    Update WebSocketConfig.java

--- src/main/java/br/com/isaccanedo/websocketchat/config/WebSocketConfig.java
+++ src/main/java/br/com/isaccanedo/websocketchat/config/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package br.com.isaccanedo.websocketchat.config;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
